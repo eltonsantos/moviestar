d9cb6a08491cbe135714f7f2ef2af03974320b5a
Elton Santos <eltonaxl@hotmail.com>
2022-07-31 04:15:16 -0300

    Configuring to use database production

--- db.php
+++ db.php
@@ -1,11 +1,16 @@
 <?php
 
   session_start();
-
-  $db_name = 'moviestar';
-  $db_host = 'localhost';
-  $db_user = 'root';
-  $db_pass = 'root';
+  // To production
+  $db_name = getenv('DB_NAME');
+  $db_host = getenv('DB_HOST');
+  $db_user = getenv('DB_USER');
+  $db_pass = getenv('DB_PASS');
+  
+  // $db_name = 'moviestar';
+  // $db_host = 'localhost';
+  // $db_user = 'root';
+  // $db_pass = 'root';
 
   $conn = new PDO("mysql:dbname=".$db_name.";host=".$db_host, $db_user, $db_pass);
 
